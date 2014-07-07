46d3c5152c2809485d27d010aff4f2a5f45c9467
IRCTCBookingAssistant <IRCTCBookingAssistant@gmail.com>
2014-07-07 22:38:23 +0530

    content script with angular js POC

--- contentscript/ContentScript.js
+++ contentscript/ContentScript.js
@@ -1,1 +1,14 @@
-/*Booking Engine.js*/
+;(function(angular,chrome) {
+	"use strict";
+	chrome.extension.sendRequest({method:"getTravelPlan"},
+		(function(response){
+			$("#loginFormId").attr("ng-controller","loginCtrl");
+			$("#usernameId").attr("ng-model","response.userId")
+			//$("#usernameId").val(response.userId);
+			angular.module('booking', [])
+			  .controller('loginCtrl', function($scope) {
+			      $scope.response = response;
+			  });
+		    angular.bootstrap(document, ['booking']);
+		}));
+})(angular,chrome);
