147903681bd97f433f5e138dec537601e7fe94c0
IRCTCBookingAssistant <IRCTCBookingAssistant@gmail.com>
2014-07-09 15:25:36 +0530

    Make Options Form more dynamic

--- src/options/options.js
+++ src/options/options.js
@@ -2,10 +2,10 @@
 	'use strict';
 	angular.module("optionsApp",[])
 	.controller("optionsCtrl",["$scope","$q",function($scope,$q) {
-		$scope.travelPlan = {
-		 	"userId":"chetanreva",
-		 	"password" : " password"
-			};
+		$scope.travelPlanInfo = [
+		{name:"User Id",value:"foo123",placeholder:"User Id",type:"text"},
+		{name:"Password",value:"foo456",placeholder:"Password",type:"password"}
+		];
 		$scope.saveTravelPlan = function(){
 	        chrome.extension.sendRequest({method: "saveTravelPlan", 
 	           data:$scope.travelPlan}, 
